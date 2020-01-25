e09a75842179843a50c4f59b64d469517d28bcab
frolikk <frolikk@frolikk-sphere>
2020-01-25 21:01:02 +0700

    Added "no-edge" var declaration && directed-ness selector

--- graph/include/graph.h
+++ graph/include/graph.h
@@ -10,10 +10,18 @@
 struct _graph;
 typedef struct _graph graph;
 
+// объявление переменной, содержащей значение,
+// сигн. об отсутствии ребра в структуре данных
+// определена в файлах исходного кода структур данных
+// как 0 для невзвешенных графов
+// и как INT_MAX для взвешенных графов
+extern const int no_edge;
+
 graph *make_graph(size_t, unsigned char);
 void dest_graph(graph *);
 
 size_t vertices(graph *);
+unsigned char directed(graph *);
 
 int add_edge(graph *, size_t, size_t, int);
 int get_edge(graph *, size_t, size_t);
