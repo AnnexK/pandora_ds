894ca0b78531d13b66d713e040368a38e9c3a595
TrotzigerEmil <roadto3kmmr@gmail.com>
2020-02-03 19:04:39 +0700

    Added Floyd-Warshall APSP algorithm

---
+++ graph/floyd.c
@@ -0,0 +1,79 @@
+#include "graph.h"
+#include "iterator.h"
+#include "iograph.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define MIN(A, B) ((A) < (B) ? (A) : (B))
+
+int reroute(int *mat, size_t i, size_t k, size_t j, size_t v)
+{
+    int k_path;
+    if (mat[i*v+k] != no_edge && mat[k*v+j] != no_edge)
+	k_path = mat[i*v+k] + mat[k*v+j];
+    else
+	k_path = no_edge;
+    return k_path == no_edge ? mat[i*v+j] : MIN(mat[i*v+j], k_path);
+}
+
+int *floyd(graph *g)
+{
+    int *mat = make_adj_mat(g);
+    size_t v = vertices(g);
+    /* обработка наличия/отсутствия петель */
+    for (size_t i = 0; i < v; i++)
+	mat[i*v+i] = mat[i*v+i] == no_edge || mat[i*v+i] >= 0 ? 0 : INT_MIN;
+    
+    for (size_t k = 0; k < v; k++)
+	for (size_t i = 0; i < v; i++)
+	    for (size_t j = 0; j < v; j++)
+		mat[i*v+j] = reroute(mat, i, k, j, v);
+    return mat;
+}
+
+void print_mat(int *mat, size_t d, int width)
+{
+    for (size_t i = 0; i < d; i++)
+    {
+	for (size_t j = 0; j < d; j++)
+	{
+	    if (mat[i*d+j] != no_edge)
+		printf("%*d", width, mat[i*d+j]);
+	    else		
+		printf("%*s", width, "inf");
+	}
+	putchar('\n');
+    }	    
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+	fprintf(stderr, "usage: %s filename\n", *argv);
+	return 1;
+    }
+
+    FILE *fp = fopen(argv[1], "r");
+    if (!fp)
+    {
+	fprintf(stderr, "%s: error: could not open file\n", *argv);
+	return 1;
+    }
+    graph *g = freadg(fp);
+    if (!g)
+    {
+	fprintf(stderr, "%s: error: could not read graph\n", *argv);
+	return 1;
+    }
+    fclose(fp);
+
+    int *apsp_mat = floyd(g);
+    print_mat(apsp_mat, vertices(g), 8);
+    
+    free(apsp_mat);
+    dest_graph(g);
+    return 0;
+}
