686f008ff78f71b0a49853b8b0fda35fde03d413
TrotzigerEmil <roadto3kmmr@gmail.com>
2020-02-05 00:44:13 +0700

    Only uses nonweight adj mat DS now; unable to link with other

--- graph/warshtcdrv.c
+++ graph/warshtcdrv.c
@@ -5,36 +5,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void write_mat(int *mat, size_t s)
+void write_mat(const unsigned char *mat, size_t s)
 {
     for (size_t i = 0; i < s; i++)
     {
 	for (size_t j = 0; j < s; j++)
 	{
-	    printf("%d ", mat[i*s+j] == no_edge ? 0 : 1);
+	    printf("%hhu ", mat[i*s+j]);
 	}
 	putchar('\n');
     }
 }
 
-graph *warshall_tc(graph *g)
-{
-    int *mat = make_adj_mat(g);
-    size_t v = vertices(g);
-    write_mat(mat, v);
-    for (size_t i = 0; i < v; i++)
-	for (size_t j = 0; j < v; j++)
-	    for (size_t k = 0; k < v; k++)
-		mat[j*v+k] = (mat[j*v+k] != no_edge) || (mat[j*v+i] != no_edge && mat[i*v+k] != no_edge) ? !no_edge : no_edge;
-
-    graph *ret = make_graph(v, directed(g));
-    for (size_t i = 0; i < v; i++)
-	for (size_t j = 0; j < v; j++)
-	    if (mat[i*v+j] != no_edge)
-		add_edge(ret, i, j, mat[i*v+j]);
-    free(mat);
-    return ret;
-}
+/* определено *ТОЛЬКО* в src/adjmatnw.c
+   остальные СД не слинкуются
+   модифицирует исходный граф */
+const unsigned char *warshall_mat(graph *);
 
 int main(int argc, char **argv)
 {
@@ -57,12 +43,10 @@ int main(int argc, char **argv)
 	return 1;
     }
     fclose(fp);
-    
-    /* дрижер тута */
-    graph *tc = warshall_tc(g);
-    writeg(tc);
-    
-    dest_graph(tc);
+
+    // возвращает указатель на матрицу *внутри* СД
+    const unsigned char *tc = warshall_mat(g);
+    write_mat(tc, vertices(g));
     dest_graph(g);
 
     return 0;
